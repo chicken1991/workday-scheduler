b3a2eaf5b56fdb7d8ad4fa8ed2bb5a1222cac05b
benpitroff <benpitroff@gmail.com>
2022-05-05 07:40:13 -0700

    placeholder divs for formatting

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,33 @@
+// var timeBlock = document.createElement("div")
+
 //display current day
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
+$("#currentDay").text(today.format("hA, dddd, MMM Do, YYYY"));
+
+
+// for each workday hour (9am - 5pm), display a timeblock
+// TODO: Create 8 timeblocks and append them to the last? Label each with appropriate hours
+function timeBlock(){
+    var startTime = 9;
+    for(var i = 0; i <= 8; i++){
+        var blockTime = startTime + i;
+        //create a div
+        //display the time using blockTime
+        //attach a button with id=blockTime
+        console.log(moment(blockTime, "H").format("hA"));
+    }
+}
+
+timeBlock();
+
+
+
+
+//If timeblock is in past, make background-color gray
+//if timeblock is the current hour, make background-color red
+//if timeblock is in the future, make background-color green
+
+//timeblock is a text field that can be saved
+
+//when the text field is saved, it is stored in localstorage and displayed upon reload
+
